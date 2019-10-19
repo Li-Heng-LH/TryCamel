1d83ab9255655ba0ae9eb6159825aaf09f12ebd2
Li Heng <liheng6567@gmail.com>
2019-10-19 22:18:10 +0800

    Print out all headers in DownloadLogger

--- src/main/java/com/me/TryCamel/DownloadLogger.java
+++ src/main/java/com/me/TryCamel/DownloadLogger.java
@@ -3,14 +3,21 @@ package com.me.TryCamel;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import java.util.Map;
+
 public class DownloadLogger implements Processor {
 
     public void process(Exchange exchange) throws Exception {
+        System.out.println("We just downloaded: "
+                + exchange.getIn().getHeader("CamelFileName"));
+
         System.out.println("MessageID: "
                 + exchange.getIn().getMessageId());
 
-        System.out.println("We just downloaded: "
-                + exchange.getIn().getHeader("CamelFileName"));
+        System.out.println("The headers are: ");
+        for (Map.Entry<String, Object> entry : exchange.getIn().getHeaders().entrySet()) {
+            System.out.println(entry);
+        }
 
         System.out.println("The content is: \n"
                 + exchange.getIn().getBody(String.class));
