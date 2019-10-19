faa578f8eadc57302316d1a89a6f6ccd5feb8b57
Li Heng <liheng6567@gmail.com>
2019-10-19 15:24:08 +0800

    Print out body in InMessage as String

---
+++ README.md
@@ -0,0 +1,5 @@
+To run this Spring DSL application:
+
+```
+mvn clean compile camel:run
+```
--- src/main/java/com/me/TryCamel/DownloadLogger.java
+++ src/main/java/com/me/TryCamel/DownloadLogger.java
@@ -6,7 +6,13 @@ import org.apache.camel.Processor;
 public class DownloadLogger implements Processor {
 
     public void process(Exchange exchange) throws Exception {
+        System.out.println("MessageID: "
+                + exchange.getIn().getMessageId());
+
         System.out.println("We just downloaded: "
                 + exchange.getIn().getHeader("CamelFileName"));
+
+        System.out.println("The content is: \n"
+                + exchange.getIn().getBody(String.class));
     }
 }
