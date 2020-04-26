39b6401a6c7dd783f604244adccc33cc372fc02f
Li Heng <liheng6567@gmail.com>
2020-04-26 23:55:13 +0800

    Anonymous Inner Class

--- chap1/FileCopier/src/main/java/me/liheng/FileCopier/FileCopier.java
+++ chap1/FileCopier/src/main/java/me/liheng/FileCopier/FileCopier.java
@@ -8,11 +8,14 @@ public class FileCopier {
 
     public static void main(String[] args) throws Exception {
         CamelContext context = new DefaultCamelContext();
+
+        //Note: using Anonymous Inner Class here
         context.addRoutes( new RouteBuilder() {
             public void configure() {
                 from("file:data/inbox?noop=true").to("file:data/outbox"); //Important: Do NOT leave space behind colon
             }
         });
+
         context.start();
         Thread.sleep(5000); //allow this simple Camel application time to copy the files
         context.stop();
